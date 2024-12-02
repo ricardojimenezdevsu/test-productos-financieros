316665ec9f046561f8f4cad17ce9bc6735210aa9
Ricardo Jimenez <ricardo.jimenez@devsu.com>
2024-12-02 12:56:06 -0700

    add jest config and remove karma

---
+++ jest.config.ts
@@ -0,0 +1,23 @@
+module.exports = {
+    displayName: "bp",
+    setupFilesAfterEnv: ["<rootDir>/src/setup-jest.ts"],
+    cacheDirectory: "<rootDir>/jest/cache",
+    globals: {},
+    clearMocks: true,
+    collectCoverage: true,
+    coverageDirectory: "<rootDir>/jest/report",
+    roots: ["<rootDir>"],
+    modulePaths: ["<rootDir>"],
+    preset: "jest-preset-angular",
+    transform: {
+        "^.+\\.(ts|js|html)$": [
+            "jest-preset-angular",
+            {
+                tsConfig: "<rootDir>/tsconfig.spec.json",
+                stringifyContentPathRegex: "\\.(html|svg)$",
+            }
+        ]
+    },
+    transformIgnorePatterns: ["node_modules/(?!.*\\.mjs$)"],
+    testRunner: "jest-circus/runner"
+}
--- src/app/api/bp.service.ts
+++ src/app/api/bp.service.ts
@@ -1,9 +1,36 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+type ApiFinancialProduct = {
+  id: string;
+  name: string;
+  description: string;
+  logo: string;
+  date_release: Date;
+  date_revision: Date;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class BpService {
+  private _baseUrl = 'http://localhost:3002/bp/products';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getFinancialProducts() {
+    return this.http.get(`${this._baseUrl}`)
+  }
+
+  createFinancialProduct( payload: ApiFinancialProduct) {
+    return this.http.post(`${this._baseUrl}`,payload)
+  }
+
+  updateFinancialProduct(id: string, payload: ApiFinancialProduct) {
+    return this.http.patch(`${this._baseUrl}/${id}`,payload)
+  }
+
+  deleteFinancialProduct(id: string) {
+    return this.http.delete(`${this._baseUrl}/${id}`)
+  }
 }
