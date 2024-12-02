9eefbcbef124eb36c999102317adf0edfce74475
Ricardo Jimenez <ricardo.jimenez@devsu.com>
2024-12-02 16:35:21 -0700

    add skeleton for product list and navigation to add product

--- src/app/financial-products/financial-products-list/financial-products-list.component.spec.ts
+++ src/app/financial-products/financial-products-list/financial-products-list.component.spec.ts
@@ -1,23 +1,36 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { provideRouter } from '@angular/router';
 import { FinancialProductsListComponent } from './financial-products-list.component';
+import { createComponentFactory, Spectator } from '@ngneat/spectator';
+import { routes } from '../../app.routes';
 
 describe('FinancialProductsListComponent', () => {
   let component: FinancialProductsListComponent;
-  let fixture: ComponentFixture<FinancialProductsListComponent>;
+  let fixture: Spectator<FinancialProductsListComponent>;
+  const createComponent = createComponentFactory({
+    component: FinancialProductsListComponent,
+    providers: [provideRouter(routes)],
+  });
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [FinancialProductsListComponent]
-    })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(FinancialProductsListComponent);
-    component = fixture.componentInstance;
+  beforeEach(() => {
+    fixture = createComponent();
+    component = fixture.component;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display initial page', () => {
+    expect(fixture.query('[qa-id="add-financial-product"]')).toBeTruthy();
+    expect(fixture.query('[qa-id="search-financial-product"]')).toBeTruthy();
+    expect(fixture.query('[qa-id="financial-products-list"]')).toBeTruthy();
+  });
+
+  it('should navigate to add new product when click on add button', () => {
+    const navigateSpy = jest.spyOn(component['router'], 'navigate');
+
+    fixture.click('[qa-id="add-financial-product"]');
+    expect(navigateSpy).toHaveBeenCalledWith(['/products/add']);
   });
 });
--- src/app/financial-products/financial-products-list/financial-products-list.component.ts
+++ src/app/financial-products/financial-products-list/financial-products-list.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-financial-products-list',
   imports: [],
   templateUrl: './financial-products-list.component.html',
-  styleUrl: './financial-products-list.component.css'
+  styleUrl: './financial-products-list.component.css',
 })
 export class FinancialProductsListComponent {
+  constructor(private router: Router) {}
 
+  onAddNewProduct() {
+    this.router.navigate(['/products/add']);
+  }
 }
