b2542d949f49ed58cd92567fe675768207a2b135
Ricardo Jimenez <ricardo.jimenez@devsu.com>
2024-12-02 17:00:16 -0700

    add form skeleton

--- src/app/financial-products/financial-product-form/financial-product-form.component.spec.ts
+++ src/app/financial-products/financial-product-form/financial-product-form.component.spec.ts
@@ -1,19 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FinancialProductFormComponent } from './financial-product-form.component';
+import { createComponentFactory, Spectator } from '@ngneat/spectator';
+import { provideRouter } from '@angular/router';
+import { routes } from '../../app.routes';
 
 describe('FinancialProductFormComponent', () => {
   let component: FinancialProductFormComponent;
-  let fixture: ComponentFixture<FinancialProductFormComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [FinancialProductFormComponent]
-    })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(FinancialProductFormComponent);
-    component = fixture.componentInstance;
+  let fixture: Spectator<FinancialProductFormComponent>;
+  const createComponent = createComponentFactory({
+    component: FinancialProductFormComponent,
+    providers: [provideRouter(routes)],
+  });
+  beforeEach(() => {
+    fixture = createComponent();
+    component = fixture.component;
     fixture.detectChanges();
   });
 
--- src/app/financial-products/financial-product-form/financial-product-form.component.ts
+++ src/app/financial-products/financial-product-form/financial-product-form.component.ts
@@ -1,11 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-financial-product-form',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './financial-product-form.component.html',
-  styleUrl: './financial-product-form.component.css'
+  styleUrl: './financial-product-form.component.css',
 })
-export class FinancialProductFormComponent {
+export class FinancialProductFormComponent implements OnInit {
+  productForm = new FormGroup({
+    // TODO: add async validation for duplicated name
+    id: new FormControl('', {
+      validators: [Validators.minLength(3), Validators.maxLength(10)],
+      nonNullable: true,
+    }),
+    name: new FormControl('', {
+      validators: [Validators.minLength(5), Validators.maxLength(100)],
+      nonNullable: true,
+    }),
+    description: new FormControl('', {
+      validators: [Validators.minLength(10), Validators.maxLength(200)],
+      nonNullable: true,
+    }),
+    logo: new FormControl('', {
+      validators: Validators.required,
+      nonNullable: true,
+    }),
+    // TODO: add custom validator for current date
+    releaseDate: new FormControl('', {
+      validators: [Validators.required],
+      nonNullable: true,
+    }),
+    revisionDate: new FormControl('', {
+      validators: [Validators.required],
+      nonNullable: true,
+    }),
+  });
 
+  constructor(private router: Router, private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      if (params && params['productId']) {
+        // TODO: edit product fill form
+      }
+    });
+  }
 }
